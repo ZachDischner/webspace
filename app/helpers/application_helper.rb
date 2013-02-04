8c70fa40f19f53012b21837db1ed866753cabd23
kevin <dinkelk@gmail.commm>
2013-02-04 10:17:06 -0700

    fully functioning website on rails!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,16 @@ module ApplicationHelper
     if page_title.empty?
       base_title
     else
-      '#{base_title} | #{page_title}'
+      "#{base_title} | #{page_title}"
     end
   end
+
+  def addColon(page_title)
+  	if page_title.empty?
+  		""
+  	else
+  		": "
+  	end
+  end
+
 end
